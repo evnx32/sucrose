3a34c65f8c7871b11841f4646fa8107f7c405792
ExceptionEvader <evnx32@gmail.com>
2023-12-01 17:27:04 +0700

    Update xendit_endpoint.dart

--- lib/src/xendit/services/endpoint/xendit_endpoint.dart
+++ lib/src/xendit/services/endpoint/xendit_endpoint.dart
@@ -1,8 +1,10 @@
 class XenditEndpoint {
   static const String BASE_URL = "https://api.xendit.co";
 
-  // Invoice
+  // Create Invoice
   String createInvoice() => "$BASE_URL/v2/invoices";
+
+  // Get Invoice by Id
   String getInvoice(String invoice_id) => "$BASE_URL/v2/invoices/$invoice_id";
 
   // Qr Payment Request
@@ -10,4 +12,16 @@ class XenditEndpoint {
 
   // Get Payment Request by Id
   String getPaymentRequest(String id) => "$BASE_URL/payment_requests/$id";
+
+  // Get List of Payment Request
+  String getListPaymentRequest() => "$BASE_URL/payment_requests";
+
+  // Get PaymentMethod by Id
+  String getPaymentMethod(String id) => "$BASE_URL/v2/payment_methods/$id";
+
+  // Get List of Payment Method
+  String getListPaymentMethod() => "$BASE_URL/v2/payment_methods";
+
+  // Create Customer
+  String createCustomer() => "$BASE_URL/customers";
 }
