8917b7798e43137b9fbb7577d6da9f22089bbc6e
ExceptionEvader <evnx32@gmail.com>
2023-12-04 16:43:02 +0700

    Refactoring

--- lib/src/xendit/services/exception/xendit_exception.dart
+++ lib/src/xendit/services/exception/xendit_exception.dart
@@ -1,16 +1,60 @@
-// Xendit Exception
-// Base class for all Xendit exceptions will return XenditError object
+// To parse this JSON data, do
+//
+//     final xenditException = xenditExceptionFromJson(jsonString);
 
-class XenditException implements Exception {
-  late Map error;
+import 'dart:convert';
 
-  XenditException(this.error);
+XenditException xenditExceptionFromJson(String str) =>
+    XenditException.fromJson(json.decode(str));
 
-  Map toMap() {
-    return error;
-  }
+String xenditExceptionToJson(XenditException data) =>
+    json.encode(data.toJson());
 
-  Map toJson() {
-    return error;
-  }
+class XenditException {
+  final String? errorCode;
+  final String? message;
+  final List<Error>? errors;
+
+  XenditException({
+    this.errorCode,
+    this.message,
+    this.errors,
+  });
+
+  factory XenditException.fromJson(Map<String, dynamic> json) =>
+      XenditException(
+        errorCode: json["error_code"],
+        message: json["message"],
+        errors: json["errors"] == null
+            ? []
+            : List<Error>.from(json["errors"]!.map((x) => Error.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "error_code": errorCode,
+        "message": message,
+        "errors": errors == null
+            ? []
+            : List<dynamic>.from(errors!.map((x) => x.toJson())),
+      };
+}
+
+class Error {
+  final String? path;
+  final String? message;
+
+  Error({
+    this.path,
+    this.message,
+  });
+
+  factory Error.fromJson(Map<String, dynamic> json) => Error(
+        path: json["path"],
+        message: json["message"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "path": path,
+        "message": message,
+      };
 }
