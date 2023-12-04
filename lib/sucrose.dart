b752d8cebfd2990b6565f88c8bf5c6549d200334
ExceptionEvader <evnx32@gmail.com>
2023-12-04 16:43:32 +0700

    Update sucrose.dart

--- lib/sucrose.dart
+++ lib/sucrose.dart
@@ -4,4 +4,4 @@ export 'src/sucrose.dart';
 
 export 'src/xendit/enums/enums.dart';
 export 'src/xendit/services/exception/xendit_exception.dart';
-export 'src/xendit/models/models.dart';
+export 'src/xendit/models/xendit_models.dart';
