0ab0eee5b94fb6461b0b368c236bc0d05f38cbca
ExceptionEvader <evnx32@gmail.com>
2023-12-04 16:39:22 +0700

    Adding some properties

--- lib/src/xendit/models/entity/channel_properties/xendit_direct_debit_channel_properties.dart
+++ lib/src/xendit/models/entity/channel_properties/xendit_direct_debit_channel_properties.dart
@@ -1,10 +1,18 @@
 class XenditDirectDebitChannelProperties {
   final String? failureReturnUrl;
   final String? successReturnUrl;
+  final String? mobileNumber;
+  final String? cardLastFour;
+  final String? cardExpiry;
+  final String? email;
 
   XenditDirectDebitChannelProperties({
     this.failureReturnUrl,
     this.successReturnUrl,
+    this.mobileNumber,
+    this.cardLastFour,
+    this.cardExpiry,
+    this.email,
   });
 
   factory XenditDirectDebitChannelProperties.fromJson(
@@ -12,10 +20,18 @@ class XenditDirectDebitChannelProperties {
       XenditDirectDebitChannelProperties(
         failureReturnUrl: json["failure_return_url"],
         successReturnUrl: json["success_return_url"],
+        mobileNumber: json["mobile_number"],
+        cardLastFour: json["card_last_four"],
+        cardExpiry: json["card_expiry"],
+        email: json["email"],
       );
 
   Map<String, dynamic> toJson() => {
-        "failure_return_url": failureReturnUrl,
-        "success_return_url": successReturnUrl,
+        if (mobileNumber != null) "mobile_number": mobileNumber,
+        if (failureReturnUrl != null) "failure_return_url": failureReturnUrl,
+        if (successReturnUrl != null) "success_return_url": successReturnUrl,
+        if (cardLastFour != null) "card_last_four": cardLastFour,
+        if (cardExpiry != null) "card_expiry": cardExpiry,
+        if (email != null) "email": email,
       };
 }
