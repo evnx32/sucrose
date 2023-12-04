57442b0fab614cb2bcc94f4c047e9ccb385367f5
ExceptionEvader <evnx32@gmail.com>
2023-12-04 16:41:55 +0700

    Create xendit_models.dart

---
+++ lib/src/xendit/models/xendit_models.dart
@@ -0,0 +1,48 @@
+export 'entity/xendit_customer_notification_preference.dart';
+export 'entity/xendit_invoice_customer.dart';
+export 'entity/xendit_invoice_item.dart';
+export 'entity/xendit_fee.dart';
+export 'entity/xendit_address.dart';
+export 'entity/xendit_qr_code_response.dart';
+export 'entity/xendit_payment_method.dart';
+export 'entity/xendit_direct_debit.dart';
+export 'entity/xendit_ewallet.dart';
+export 'entity/xendit_metadata.dart';
+export 'entity/xendit_billing_information.dart';
+//export 'entity/xendit_retail_outlet.dart';
+export 'entity/xendit_virtual_account.dart';
+export 'entity/xendit_card.dart';
+export 'entity/xendit_qr_channel_properties.dart';
+export 'entity/xendit_customer.dart';
+export 'entity/xendit_individual_detail.dart';
+export 'entity/xendit_business_detail.dart';
+export 'entity/xendit_ewallet_account.dart';
+
+export 'entity/payment_method/xendit_qr_payment_method.dart';
+export 'entity/payment_method/xendit_va_payment_method.dart';
+export 'entity/payment_method/xendit_ewallet_payment_method.dart';
+export 'entity/payment_method_props/xendit_ewallet_props.dart';
+export 'entity/payment_method_props/xendit_qr_code_props.dart';
+export 'entity/payment_method_props/xendit_virtual_account_props.dart';
+
+// channel properties
+export 'entity/channel_properties/xendit_card_channel_properties.dart';
+export 'entity/channel_properties/xendit_va_channel_properties.dart';
+export 'entity/channel_properties/xendit_ewallet_channel_properties.dart';
+export 'entity/channel_properties/xendit_direct_debit_channel_properties.dart';
+
+// request
+export 'request/request_payment/xendit_payment_method_request.dart';
+export 'request/request_payment/virtual_account/xendit_virtual_account_request.dart';
+export 'request/request_payment/qr/xendit_qr_request.dart';
+export 'request/request_payment/xendit_request_payment.dart';
+export 'request/request_payment/ewallet/xendit_ewallet_request.dart';
+export 'request/invoice/xendit_invoice_request.dart';
+export 'request/linking/ewallet/xendit_ewallet_linking_request.dart';
+export 'request/linking/ewallet/xendit_update_ewallet_request.dart';
+
+// response
+export 'response/general/xendit_payment_request_by_id_response.dart';
+export 'response/request_payment/xendit_request_payment_response.dart';
+export 'response/general/ewallet/xendit_ewallet_response.dart';
+export 'response/invoice/xendit_invoice_response.dart';
