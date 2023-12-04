b9da1dbd8ad99976731502535993b85a70d0a865
ExceptionEvader <evnx32@gmail.com>
2023-12-04 16:42:35 +0700

    Adding some endpoints and refactoring some functions

--- lib/src/xendit/services/endpoint/xendit_endpoint.dart
+++ lib/src/xendit/services/endpoint/xendit_endpoint.dart
@@ -7,8 +7,11 @@ class XenditEndpoint {
   // Get Invoice by Id
   String getInvoice(String invoiceId) => "$BASE_URL/v2/invoices/$invoiceId";
 
-  // Qr Payment Request
+  // Payment Request
   String paymentRequest() => "$BASE_URL/payment_requests";
+
+  // Payment Method
+  String paymentMethod() => "$BASE_URL/v2/payment_methods";
 
   // Get Payment Request by Id
   String getPaymentRequest(String id) => "$BASE_URL/payment_requests/$id";
@@ -16,9 +19,19 @@ class XenditEndpoint {
   // Get PaymentMethod by Id
   String getPaymentMethod(String id) => "$BASE_URL/v2/payment_methods/$id";
 
-  // Get List of Payment Method
-  String getListPaymentMethod() => "$BASE_URL/v2/payment_methods";
-
   // Create Customer
   String createCustomer() => "$BASE_URL/customers";
+
+  // Patch Payment Method
+  String patchPaymentMethod(String id) => "$BASE_URL/v2/payment_methods/$id";
+
+  // Expire Payment Method
+  String expirePaymentMethod(String id) =>
+      "$BASE_URL/v2/payment_methods/$id/expire";
+
+  // Refund
+  String refund() => "$BASE_URL/refunds";
+
+  // Get Refund by Id
+  String getRefund(String id) => "$BASE_URL/refunds/$id";
 }
