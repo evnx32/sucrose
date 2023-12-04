c1a8de119a8d531dca38bd7cc2b34f559fde4444
ExceptionEvader <evnx32@gmail.com>
2023-12-04 16:40:48 +0700

    Refactoring

--- lib/src/xendit/models/entity/xendit_ewallet.dart
+++ lib/src/xendit/models/entity/xendit_ewallet.dart
@@ -4,7 +4,9 @@
 
 import 'dart:convert';
 
+import 'package:sucrose/src/xendit/enums/enums.dart';
 import 'package:sucrose/src/xendit/models/entity/channel_properties/xendit_ewallet_channel_properties.dart';
+import 'package:sucrose/src/xendit/models/entity/xendit_ewallet_account.dart';
 
 XenditEwallet xenditEwalletFromJson(String str) =>
     XenditEwallet.fromJson(json.decode(str));
@@ -12,57 +14,30 @@ XenditEwallet xenditEwalletFromJson(String str) =>
 String xenditEwalletToJson(XenditEwallet data) => json.encode(data.toJson());
 
 class XenditEwallet {
-  final Account? account;
-  final String? channelCode;
-  final XenditEwalletChannelProperties? channelProperties;
+  final XenditEwalletAccount? account;
+  final XenditEWalletCode channelCode;
+  final XenditEwalletChannelProperties channelProperties;
 
   XenditEwallet({
     this.account,
-    this.channelCode,
-    this.channelProperties,
+    required this.channelCode,
+    required this.channelProperties,
   });
 
   factory XenditEwallet.fromJson(Map<String, dynamic> json) => XenditEwallet(
-        account:
-            json["account"] == null ? null : Account.fromJson(json["account"]),
-        channelCode: json["channel_code"],
-        channelProperties: json["channel_properties"] == null
+        account: json["account"] == null
             ? null
-            : XenditEwalletChannelProperties.fromJson(
-                json["channel_properties"]),
+            : XenditEwalletAccount.fromJson(json["account"]),
+        channelCode: XenditEWalletCode.values.firstWhere(
+          (element) => element.name == json["channel_code"],
+        ),
+        channelProperties:
+            XenditEwalletChannelProperties.fromJson(json["channel_properties"]),
       );
 
   Map<String, dynamic> toJson() => {
-        "account": account?.toJson(),
-        "channel_code": channelCode,
-        "channel_properties": channelProperties?.toJson(),
-      };
-}
-
-class Account {
-  final String? name;
-  final dynamic balance;
-  final dynamic pointBalance;
-  final String? accountDetails;
-
-  Account({
-    this.name,
-    this.balance,
-    this.pointBalance,
-    this.accountDetails,
-  });
-
-  factory Account.fromJson(Map<String, dynamic> json) => Account(
-        name: json["name"],
-        balance: json["balance"],
-        pointBalance: json["point_balance"],
-        accountDetails: json["account_details"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "name": name,
-        "balance": balance,
-        "point_balance": pointBalance,
-        "account_details": accountDetails,
+        if (account != null) "account": account!.toJson(),
+        "channel_code": channelCode.name,
+        "channel_properties": channelProperties.toJson(),
       };
 }
