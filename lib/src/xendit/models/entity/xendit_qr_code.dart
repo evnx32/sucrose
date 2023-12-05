8d6cfcb263ed353e361ea7835331b835c211dd6f
ExceptionEvader <evnx32@gmail.com>
2023-12-05 18:31:29 +0700

    Create xendit_qr_code.dart

---
+++ lib/src/xendit/models/entity/xendit_qr_code.dart
@@ -0,0 +1,53 @@
+import 'package:sucrose/src/xendit/enums/enums.dart';
+import 'package:sucrose/src/xendit/models/entity/channel_properties/xendit_qr_channel_properties.dart';
+
+class XenditQrCode {
+  /// Expected and accepted amount of the transaction in the actual value in the provided currency.
+  ///
+  /// Decimal places support varies per currency:
+  ///
+  /// IDR - Only supports positive integers.
+  ///
+  /// VND - Only supports positive integers.
+  ///
+  /// THB - Supports up to two decimal places.
+  final int? amount;
+
+  /// ISO 4217 three-letter code of the transaction's currency. Will be auto-filled based on the channel_code if not provided.
+  final XenditQRCurrency currency;
+
+  /// Identifier for the QR Code standard used. This parameter is not required for Indonesia channel code.
+  final XenditQRChannelCode channelCode;
+
+  /// Information regarding the payment method / instrument
+  final XenditQRChannelProperties? channelProperties;
+
+  /// Object that contains the required information to perform payments with QR code
+  XenditQrCode({
+    this.amount,
+    required this.currency,
+    required this.channelCode,
+    this.channelProperties,
+  });
+
+  factory XenditQrCode.fromJson(Map<String, dynamic> json) => XenditQrCode(
+        amount: json["amount"],
+        currency: XenditQRCurrency.values.firstWhere(
+          (element) => element.name == json["currency"],
+        ),
+        channelCode: XenditQRChannelCode.values.firstWhere(
+          (element) => element.name == json["channel_code"],
+        ),
+        channelProperties: json["channel_properties"] == null
+            ? null
+            : XenditQRChannelProperties.fromJson(json["channel_properties"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        if (amount != null) "amount": amount,
+        "currency": currency.name,
+        "channel_code": channelCode.name,
+        if (channelProperties != null)
+          "channel_properties": channelProperties!.toJson(),
+      };
+}
