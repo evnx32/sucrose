e2f2dc2b04f0cdcc1c92148f51e5012bb5704623
ExceptionEvader <evnx32@gmail.com>
2023-12-13 17:44:18 +0700

    Adding init Midtrans

--- lib/src/sucrose.dart
+++ lib/src/sucrose.dart
@@ -1,5 +1,5 @@
-// Artic is Indonesian payment gateway bridge for Flutter
-
+import 'package:sucrose/src/midtrans/enums/enums.dart';
+import 'package:sucrose/src/midtrans/services/http/midtrans_http_request.dart';
 import 'package:sucrose/src/xendit/services/http/xendit_http_request.dart';
 
 import 'dart:convert';
@@ -7,11 +7,26 @@ import 'dart:convert';
 class Sucrose {
   // xendit services
   late XenditHttpRequest xendit;
+  late MidtransHttpRequest midtrans;
 
   /// Initialize Xendit with Xendit API key
   Sucrose.initXendit({required String xenditApiKey, bool debug = false}) {
     xendit = XenditHttpRequest.init(
-        "Basic ${base64.encode(utf8.encode("$xenditApiKey:"))}",
-        debug: debug);
+      "Basic ${base64.encode(utf8.encode("$xenditApiKey:"))}",
+      debug: debug,
+    );
+  }
+
+  /// Initialize Midtrans with Midtrans API key
+  Sucrose.initMidtrans({
+    required String midtransApiKey,
+    required MidtransEnvironment environment,
+    bool debug = false,
+  }) {
+    midtrans = MidtransHttpRequest.init(
+      apiKey: "Basic ${base64.encode(utf8.encode("$midtransApiKey:"))}}",
+      environment: environment,
+      debug: debug,
+    );
   }
 }
