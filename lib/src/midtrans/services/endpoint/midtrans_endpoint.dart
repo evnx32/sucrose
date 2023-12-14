cf6a204cc661ae1d28250470f6832af9f53741fb
ExceptionEvader <evnx32@gmail.com>
2023-12-14 17:14:36 +0700

    Adding some endpoints

--- lib/src/midtrans/services/endpoint/midtrans_endpoint.dart
+++ lib/src/midtrans/services/endpoint/midtrans_endpoint.dart
@@ -9,4 +9,19 @@ class MidtransEndpoint {
     String cardCvv,
   ) =>
       "/v2/token?client_key=$clientKey&card_number=$cardNumber&card_exp_month=$cardExpMonth&card_exp_year=$cardExpYear&card_cvv=$cardCvv";
+
+  // Cancel Transaction
+  String cancelTransaction(String id) => "/v2/$id/cancel";
+
+  // Expire Transaction
+  String expireTransaction(String id) => "/v2/$id/expire";
+
+  // Refund Transaction
+  String refundTransaction(String id) => "/v2/$id/refund";
+
+  // Transaction Status
+  String transactionStatus(String id) => "/v2/$id/status";
+
+  // Snap Transaction
+  String snapTransaction() => "/snap/v1/transactions";
 }
